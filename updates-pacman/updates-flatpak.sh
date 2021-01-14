571ec30ea6f489bb2aa70dfb8e6f15c6f6b6c9e8
trastolillo <trastolillo@tutanota.com>
2021-01-14 12:48:27 +0100

    updates module

--- updates-pacman/updates-flatpak.sh
+++ updates-pacman/updates-flatpak.sh
@@ -3,7 +3,7 @@
 updates=$(echo 'n' | flatpak update 2>/dev/null | tail -n +5 | head -2 | wc -l)
 
 if [ "$updates" -gt 0 ]; then
-    echo "flatpak: $updates"
+    echo "$updates"
 else
     echo ""
 fi
